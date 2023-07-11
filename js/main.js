06ca253c43f228b82f7ee0a86f364fbda7902b03
no-on-zine <60006483+no-on-zine@users.noreply.github.com>
2023-07-11 16:36:35 +0200

    16:36

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ jQuery(function ($) {
     .not(notElement)
     .on('inview', function (event, isInView, visiblePartX, visiblePartY) {
       if (isInView) {
-        $(this).stop().addClass('active');
+        $(this).stop().addClass('i');
       }
     });
 });
