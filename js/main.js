6519e616b6757d73e4bb8f39cc23afebe278f3ff
no-on-zine <60006483+no-on-zine@users.noreply.github.com>
2023-09-18 23:21:08 +0100

    js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+jQuery(function ($) {
+  const notElement =
+    'html,body,section,h1,h2,img,a,header,footer,main,br,.no_inview';
+  $('*')
+    .not(notElement)
+    .on('inview', function (event, isInView, visiblePartX, visiblePartY) {
+      if (isInView) {
+        $(this).stop().addClass('i');
+      }
+    });
+});
+
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
