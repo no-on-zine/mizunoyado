b54df5a6c46c8a0d42b3856e80e582997fc5da9e
no-on-zine <60006483+no-on-zine@users.noreply.github.com>
2023-09-18 23:19:03 +0100

    jsを一旦消去

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-jQuery(function ($) {
-  const notElement =
-    'html,body,section,h1,h2,img,a,header,footer,main,br,.no_inview';
-  $('*')
-    .not(notElement)
-    .on('inview', function (event, isInView, visiblePartX, visiblePartY) {
-      if (isInView) {
-        $(this).stop().addClass('i');
-      }
-    });
-});
-
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
@@ -21,5 +9,3 @@ window.addEventListener('resize', () => {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
-
-var rellax = new Rellax('.rellax');
