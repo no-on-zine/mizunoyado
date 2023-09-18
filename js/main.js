c5664e47a6389e4fd74b600ccfd834cb77ba32ea
no-on-zine <60006483+no-on-zine@users.noreply.github.com>
2023-09-18 23:22:17 +0100

    no_inview

--- js/main.js
+++ js/main.js
@@ -21,3 +21,5 @@ window.addEventListener('resize', () => {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
+
+var rellax = new Rellax('.rellax');
